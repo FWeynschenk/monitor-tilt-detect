76dbbe794de513876b0225889e376486b08b3fbc
FWeynschenk <f.l.weynschenk@student.tudelft.nl>
2022-07-05 23:00:24 +0200

    small changes for new setup

--- src/hostBackground.py
+++ src/hostBackground.py
@@ -5,69 +5,75 @@ import screen_brightness_control as sbc
 
 def initSerial(dev):
     ser = serial.Serial(dev, 9600, timeout=1, xonxoff=False, rtscts=False, dsrdtr=False)
-    ser.flushInput()
-    ser.flushOutput()
+    ser.reset_input_buffer()
+    ser.reset_output_buffer()
     return ser
 
 def waitForSerialInit():
-    possibleDevices = ["COM5"]
+    possibleDevices = ["COM3"]
     while True:
         for dev in possibleDevices:
             try:
                 ser = initSerial(dev)
                 print("device found on " + dev)
                 return ser
-            except Exception:
+            except Exception as e:
+                print(e)
                 print("Failed to initialise device on " + dev)
         time.sleep(5)
 
 ser = waitForSerialInit()
 
-monitor = rotatescreen.get_secondary_displays()[0]
-landscape = monitor.current_orientation == 0
+rotatableMonitor = rotatescreen.get_secondary_displays()[0]
 
 while True:
     try:
         line = ser.readline().decode("utf-8")
+        landscape = rotatableMonitor.current_orientation == 0
+        
+        if line == "":
+            continue # ignore empty lines
+
+        # ROTATION
+        if (line.find("command: landscape") != -1  and not landscape):
+            try:
+                rotatableMonitor.set_landscape()
+            except Exception:
+                continue
+                
+        if (line.find("command: portrait") != -1 and landscape):
+            try:
+                rotatableMonitor.set_portrait_flipped()
+            except Exception:
+                continue
+
+        # BRIGHTNESS
+        if (line.find("brightness: ") != -1):
+            cbr = -1
+            try:
+                cbr = sbc.get_brightness(display=0)
+            except Exception:
+                continue
+
+            nbr = int(line.replace("brightness: ", ""))
+            if(nbr != cbr):
+                try:
+                    sbc.set_brightness(nbr, display=0)
+                except Exception:
+                    continue
+                try:
+                    sbc.set_brightness(nbr, display=1)
+                except Exception:
+                    continue
+
+                
     
-    except Exception:
-        print("error: ")
-        print("probably not plugged in")
+    except Exception as err:
+        print(err)
         time.sleep(5)
         print("trying to init serial again")
+        ser.close()
         ser = waitForSerialInit()
         continue
 
 
-    if line == "":
-        continue # ignore empty lines
-
-    if (line.find("command: landscape") != -1):
-        if not landscape:
-            try:
-                monitor.set_landscape()
-                landscape = True
-            except Exception:
-                pass
-        
-    if (line.find("command: portrait") != -1):
-        if landscape:
-            try:
-                monitor.set_portrait_flipped()
-                landscape = False
-            except Exception:
-                pass
-
-    if (line.find("brightness: ") != -1):
-        cbr = -1
-        try:
-            cbr = sbc.get_brightness(display=0)
-        except Exception:
-            pass
-
-        nbr = int(line.replace("brightness: ", ""))
-        if(nbr != cbr):
-            try:
-                sbc.set_brightness(nbr, display=0)
-            except Exception:
-                pass
